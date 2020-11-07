8691a4a4ba1ee530b8a9be2cf670316c8c61bb0c
Leopoldo B Barreiro <leopoldo.barreiro@arphoenix.com.br>
2020-11-07 18:06:19 -0300

    Incluido mfe para rodape. Acrescentado suporte a bootstrap e single-spa layout

--- frontends/mfe1/src/root.component.js
+++ frontends/mfe1/src/root.component.js
@@ -1,7 +1,38 @@
 import React from "react";
 
 export default function Root(props) {
-  return <section>
-      <div className="mfe1">{props.name} is mounted!</div>
-    </section>;
+  return (
+    <section>
+      <div class="page-header">
+        <h1>Eu sou o mfe {props.name}</h1>
+      </div>
+      <div class="alert alert-success" role="alert">
+        <strong>Well done!</strong> You successfully read this important alert
+        message.
+      </div>
+      <p>
+        <button type="button" class="btn btn-lg btn-default">
+          Default
+        </button>
+        <button type="button" class="btn btn-lg btn-primary">
+          Primary
+        </button>
+        <button type="button" class="btn btn-lg btn-success">
+          Success
+        </button>
+        <button type="button" class="btn btn-lg btn-info">
+          Info
+        </button>
+        <button type="button" class="btn btn-lg btn-warning">
+          Warning
+        </button>
+        <button type="button" class="btn btn-lg btn-danger">
+          Danger
+        </button>
+        <button type="button" class="btn btn-lg btn-link">
+          Link
+        </button>
+      </p>
+    </section>
+  );
 }
--- frontends/rodape/src/root.component.js
+++ frontends/rodape/src/root.component.js
@@ -3,9 +3,7 @@ import React from "react";
 export default function Root(props) {
   return (
     <section>
-      <div class="page-header">
-        <h5>Eu sou o mfe {props.name}</h5>
-      </div>
+      <footer>{props.name} está rodando!</footer>
     </section>
   );
 }
